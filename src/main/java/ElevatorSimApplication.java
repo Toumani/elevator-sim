92d16ee684ae4b1e71cb112a28871f985705419e
Toumani <t.sidibi@perenitysoftware.com>
2022-03-28 16:10:36 +0000

    Initial commit

---
+++ src/main/java/ElevatorSimApplication.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ElevatorSimApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ElevatorSimApplication.class.getResource("/sample.fxml"));
+        primaryStage.setScene(new Scene(fxmlLoader.load(), 400, 300));
+        primaryStage.show();
+    }
+}
