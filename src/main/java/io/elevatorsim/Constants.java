5f1d4107e5ee6a1f4f0db253736000c945db6a8e
Toumani <t.sidibi@perenitysoftware.com>
2022-04-17 18:54:39 +0000

    ui: animate ARRIVE and ENTER actions

---
+++ src/main/java/io/elevatorsim/Constants.java
@@ -0,0 +1,8 @@
+package io.elevatorsim;
+
+import javafx.util.Duration;
+
+public class Constants {
+    public static Duration FADE_DURATION = new Duration(500);
+    public static Duration TRANSLATE_DURATION = new Duration(500);
+}
--- src/main/java/io/elevatorsim/main/FloorView.java
+++ src/main/java/io/elevatorsim/main/FloorView.java
@@ -1,8 +1,14 @@
 package io.elevatorsim.main;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -12,8 +18,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static io.elevatorsim.Constants.FADE_DURATION;
+import static io.elevatorsim.Constants.TRANSLATE_DURATION;
+
 public class FloorView extends AnchorPane implements Initializable {
     @FXML HBox queue_HBX;
+
+    private static final int USER_WIDTH = 43;
 
     public FloorView() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main/floor-view.fxml"));
@@ -30,21 +41,59 @@ public class FloorView extends AnchorPane implements Initializable {
     }
 
     public void pushRandomUser() {
-        queue_HBX.getChildren().add(createRandomUser());
+        Node newUser = createRandomUser();
+        queue_HBX.getChildren().add(newUser);
+        FadeTransition fadeTransition = new FadeTransition(FADE_DURATION, newUser);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.play();
     }
 
     public void pushNRandomUser(int n) {
         for (int i = 0; i < n; i++)
-            queue_HBX.getChildren().add(createRandomUser());
+            pushRandomUser();
     }
 
     public void popUser() {
-        try { queue_HBX.getChildren().remove(0); }
-        catch (IndexOutOfBoundsException ignored) { }
+        if (queue_HBX.getChildren().isEmpty())
+            return;
+
+        Node user = queue_HBX.getChildren().get(0);
+        FadeTransition fadeTransition = new FadeTransition(FADE_DURATION, user);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.play();
+        fadeTransition.setOnFinished((ActionEvent e) -> queue_HBX.getChildren().remove(0));
     }
 
     public void popNUser(int n) {
-        try { queue_HBX.getChildren().remove(0, n); }
+        try {
+            if (n > queue_HBX.getChildren().size())
+                return;
+
+            ParallelTransition fadeTransitions = new ParallelTransition();
+            ParallelTransition translateTransitions = new ParallelTransition();
+            for (int i = 0; i < queue_HBX.getChildren().size(); i++) {
+                Node user = queue_HBX.getChildren().get(i);
+                if (i < n) {
+                    FadeTransition fadeTransition = new FadeTransition(FADE_DURATION, user);
+                    fadeTransition.setFromValue(1.0);
+                    fadeTransition.setToValue(0.0);
+                    fadeTransitions.getChildren().add(fadeTransition);
+                }
+                else {
+                    TranslateTransition translateTransition = new TranslateTransition(TRANSLATE_DURATION.multiply(n), user);
+                    translateTransition.setByX(-USER_WIDTH*n);
+                    translateTransitions.getChildren().add(translateTransition);
+                }
+            }
+            SequentialTransition sequentialTransition = new SequentialTransition(fadeTransitions, translateTransitions);
+            sequentialTransition.setOnFinished((ActionEvent) -> {
+                queue_HBX.getChildren().remove(0, n);
+                queue_HBX.getChildren().forEach(it -> it.setTranslateX(0));
+            });
+            sequentialTransition.play();
+        }
         catch (IndexOutOfBoundsException ignored) { }
 
     }
@@ -63,8 +112,9 @@ public class FloorView extends AnchorPane implements Initializable {
         if (url == null)
             throw new RuntimeException();
         ImageView imageView = new ImageView(new Image("file://" + url.getPath()));
-        imageView.setFitHeight(43);
+        imageView.setFitWidth(USER_WIDTH);
         imageView.setPreserveRatio(true);
+        imageView.setOpacity(0.0);
         return imageView;
     }
 }
